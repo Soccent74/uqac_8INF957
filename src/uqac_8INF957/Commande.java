cb8f8bfeb76c9c9c7aa56af7028bef8f2b795b6d
Lucas <lucas.helainemenard@hotmail.fr>
2016-02-13 13:01:13 -0500

    Modification de Commande
    ...un petit bonhomme...

--- src/uqac_8INF957/Commande.java
+++ src/uqac_8INF957/Commande.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.util.Arrays;
 
 /**
- * @author lucas
+ * @author Benjamin Bourgeaux et Lucas Helaine
  *
  */
 public class Commande implements Serializable {
@@ -20,11 +20,15 @@ public class Commande implements Serializable {
 	private String nom_attribut = ""; //nom de l'attribut
 	private String valeur = ""; //valeur à écrire
 	private String nom_fonction = ""; //nom de la fonction à appeler
+	private String[] tabpar = new String[taille]; //tableau des paramètres
+	private String[] tabval = new String[taille]; //tableau des valeurs des paramètres
+	private int nb_args = 0; //nombre d'arguments dans la commande
 
 	public Commande(String commande){
 
-		String[] str = commande.split("#");
-		if(str[0].equals("compilation")){
+		String[] str = commande.split("#"); //Séparation de la commande
+		nb_args = str.length;
+		if((str[0].equals("compilation"))&&(nb_args == 3)){ //Si le premier terme est "compilation", je récupère les valeurs
 			this.type_commande = str[0];
 			String[] source = str[1].split(",");
 			for(int i = 0; i < 2; i++){
@@ -32,98 +36,110 @@ public class Commande implements Serializable {
 			}
 			this.chemin_classe = str[2];
 		}
-		else if(str[0].equals("chargement")){
+		else if((str[0].equals("chargement"))&&(nb_args == 2)){ //Si le premier terme est "compilation", je récupère les valeurs
 			this.type_commande = str[0];
 			this.nom_classe = str[1];
 		}
-		else if(str[0].equals("creation")){
+		else if((str[0].equals("creation"))&&(nb_args == 3)){ //Si le premier terme est "creation", je récupère les valeurs
 			this.type_commande = str[0];
 			this.nom_classe = str[1];
 			this.identificateur = str[2];
 		}
-		else if(str[0].equals("lecture")){
+		else if((str[0].equals("lecture"))&&(nb_args == 3)){ //Si le premier terme est "lecture", je récupère les valeurs
 			this.type_commande = str[0];
 			this.identificateur = str[1];
 			this.nom_attribut = str[2];
 		}
-		else if(str[0].equals("ecriture")){
+		else if((str[0].equals("ecriture"))&&(nb_args == 4)){ //Si le premier terme est "ecriture", je récupère les valeurs
 			this.type_commande = str[0];
 			this.identificateur = str[1];
 			this.nom_attribut = str[2];
 			this.valeur = str[3];
 		}
-		else{
-			System.out.println("Commande non traitée");
+		else if((str[0].equals("fonction"))&&(nb_args >= 3)){ //Si le premier terme est "fonction", je récupère les valeurs
+			this.type_commande = str[0];
+			this.identificateur = str[1];
+			this.nom_fonction = str[2];
+			if(nb_args == 4){
+				System.out.println(str[3]);
+				String[] param = str[3].split(",");
+				for(int i = 0; i < 2; i++){
+					String[] tampon = param[i].split(":");
+					System.out.println(tampon[i]);
+					for(int j = 0; j < 2; j++){
+						this.tabpar[i] = tampon[0];
+						this.tabval[i] = tampon[1];
+					}
+				}
+			}
+		}
+		else{ //Commande mal formée
+			System.out.println("Commande non traitée. Veuillez reprendre la normalisation de votre commande");
 		}
 	}
 
 	public void setType_commande(String type_commande) { //Setter de type_commande
-		this.type_commande = type_commande;
-	}
+		this.type_commande = type_commande;}
 	public String getType_commande() { //Getter de type_commande
-		return type_commande;
-	}
+		return type_commande;}
 
 	public void setChemin_source(String[] chemin_source) { //Setter de chemin_source
-		this.chemin_source = chemin_source;
-	}
+		this.chemin_source = chemin_source;}
 	public String[] getChemin_source() { //Getter de chemin_source
-		return chemin_source;
-	}
+		return chemin_source;}
 	
 	public void setChemin_classe(String chemin_classe) { //Setter de chemin_classe
-		this.chemin_classe = chemin_classe;
-	}
+		this.chemin_classe = chemin_classe;}
 	public String getChemin_classe() { //Getter de chemin_classe
-		return chemin_classe;
-	}
+		return chemin_classe;}
 	
 	public void setNom_classe(String nom_classe) { //Setter de nom_classe
-		this.nom_classe = nom_classe;
-	}
+		this.nom_classe = nom_classe;}
 	public String getNom_classe() { //Getter de nom_classe
-		return nom_classe;
-	}
+		return nom_classe;}
 	
 	public void setIdentificateur(String identificateur) { //Setter de identificateur
-		this.identificateur = identificateur;
-	}
+		this.identificateur = identificateur;}
 	public String getIdentificateur() { //Getter de identificateur
-		return identificateur;
-	}
+		return identificateur;}
 	
 	public void setNom_attribut(String nom_attribut) { //Setter de nom_attribut
-		this.nom_attribut = nom_attribut;
-	}
+		this.nom_attribut = nom_attribut;}
 	public String getNom_attribut() { //Getter de nom_attribut
-		return nom_attribut;
-	}
+		return nom_attribut;}
 	
 	public void setValeur(String valeur) { //Setter de valeur
-		this.valeur = valeur;
-	}
+		this.valeur = valeur;}
 	public String getValeur() { //Getter de valeur
-		return valeur;
-	}
+		return valeur;}
 	
 	public void setNom_fonction(String nom_fonction) { //Setter de nom_fonction
-		this.nom_fonction = nom_fonction;
-	}
+		this.nom_fonction = nom_fonction;}
 	public String getNom_fonction() { //Getter de nom_fonction
-		return nom_fonction;
-	}
+		return nom_fonction;}
 	
+	public void setTabpar(String[] tabpar) { //Setter de tabpar
+		this.tabpar = tabpar;}
+	public String[] getTabpar() { //Getter de tabpar
+		return tabpar;}
 	
-	public static void main (String [] arg){
-		final String defaut = "chargement#ca.uqac.registraire.Cours";
+	public void setTabval(String[] tabval) { //Setter de tabval
+		this.tabval = tabval;}
+	public String[] getTabval() { //Getter de tabval
+		return tabval;}
+	
+	/*public static void main (String [] arg){
+		final String defaut = "fonction#mathilde#getMoyenne#";
         Commande test= new Commande(defaut);
         System.out.println(test.getType_commande());
-        System.out.println(test.getNom_classe());
+        //System.out.println(test.getNom_classe());
 		//System.out.println(Arrays.toString(test.getChemin_source()));
         //System.out.println(test.getChemin_classe());
-        //System.out.println(test.getIdentificateur());
+        System.out.println(test.getIdentificateur());
         //System.out.println(test.getNom_attribut());
         //System.out.println(test.getValeur());
-   }
-
+        System.out.println(test.getNom_fonction());
+        System.out.println(Arrays.toString(test.getTabpar()));
+        System.out.println(Arrays.toString(test.getTabval()));        
+   }*/
 }
