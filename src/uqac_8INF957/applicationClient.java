0c49edc2ca87f2006e66f43ea5c91a9659f0f0ee
Benjamin <bourgeaux.benj@gmail.com>
2016-02-13 16:51:18 -0500

    Fonction INIT client
    Elle est faites !!! :)

--- src/uqac_8INF957/applicationClient.java
+++ src/uqac_8INF957/applicationClient.java
@@ -5,38 +5,93 @@ package uqac_8INF957;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import uqac_8INF957.Commande;
 /**
  * @author Benjamin Bourgeaux et Lucas Hélaine
  *
  */
 public class applicationClient {
-	public static void main(String[] zero) {
-		File streamCommande = null;
-//	    String filePath = "commandes.txt";
+    /** ATTRIBUT **/
+	private String fichCommandes = null;
+	private String fichSortie = null;
+	static BufferedReader br = null;
+	static InputStream ips = null;
+	static InputStreamReader ipsr  = null;
+	/**
+    * prend le fichier contenant la liste des commandes, et le charge dans une
+    * variable du type Commande qui est retournée
+    */         
+    public static Commande saisisCommande(String line) {
+    	Commande ordre = new Commande(line);
+		return ordre;
+    }
+    
+    /**
+    * initialise : ouvre les différents fichiers de lecture et écriture
+     * @throws FileNotFoundException 
+    */
+    public static void initialise(String fichierCommande) throws FileNotFoundException {
+	    try{	
+	    	ips = new FileInputStream(fichierCommande); 
+			ipsr = new InputStreamReader(ips);
+			br = new BufferedReader(ipsr);
+			
+	    }catch (Exception e){
+			System.out.println(e.toString());
+		}
+	
+    }
+    
+    /**
+    * prend une Commande dûment formatée, et la fait exécuter par le serveur. Le résultat de 
+    * l’exécution est retournée. Si la commande ne retourne pas de résultat, on retourne null.
+    * Chaque appel doit ouvrir une connexion, exécuter, et fermer la connexion. Si vous le 
+    * souhaitez, vous pourriez écrire six fonctions spécialisées, une par type de commande 
+	* décrit plus haut, qui seront appelées par  traiteCommande(Commande uneCommande)
+    */
+//    public Object traiteCommande(Commande uneCommande) {
+//
+//    }
+    
+    
+    /**
+    * cette méthode vous sera fournie plus tard. Elle indiquera la séquence d’étapes à exécuter
+    * pour le test. Elle fera des appels successifs à saisisCommande(BufferedReader fichier) et
+    * traiteCommande(Commande uneCommande). 
+    */
+//    public void scenario() {
+//    	sortieWriter.println("Debut des traitements:");
+//		Commande prochaine = saisisCommande(commandesReader);
+//		while (prochaine != null) {
+//			sortieWriter.println("\tTraitement de la commande " + prochaine + " ...");
+//			Object resultat = traiteCommande(prochaine);
+//			sortieWriter.println("\t\tResultat: " + resultat);
+//			prochaine = saisisCommande(commandesReader);
+//		}
+//		sortieWriter.println("Fin des traitements");
+//    }
+    
+    /**
+    * programme principal. Prend 4 arguments: 1) “hostname” du serveur, 2) numéro de port, 
+    * 3) nom fichier commandes, et 4) nom fichier sortie. Cette méthode doit créer une 
+    * instance de la classe ApplicationClient, l’initialiser, puis exécuter le scénario
+    */
 
-	    
-	    // On test si on peut ouvrir le fichier, ainsi que si il n'y a pas de problème avec les entrées/sorties. 
-		try { 
-	    	streamCommande = new File("commandes.txt");
-	    	Scanner scanner=new Scanner(streamCommande);
-	    	 
-	    	// On boucle sur chaque champ detecté
-	    	while (scanner.hasNextLine()) {
-	    	    String line = scanner.nextLine();
-	    	    System.out.println(line);
-	    		//faites ici votre traitement
-	    	    
-	    	    
-	    	    
-	    	}
-	    	scanner.close();
-	    }catch(FileNotFoundException e) {
-	         // Cette exception est levée si l'objet FileInputStream ne trouve aucun fichier
-	         e.printStackTrace();
-	    }catch(IOException e) {
-	         // Celle-ci se produit lors d'une erreur d'écriture ou de lecture
-	         e.printStackTrace();
-	    }
+    public static void main(String[] zero) {
+		String fichier ="commandes.txt";
+		
+		
+		//lecture du fichier texte	
+		try{
+			initialise(fichier);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				System.out.println(ligne);
+			}
+			br.close(); 
+		}catch (Exception e){
+			System.out.println(e.toString());
+		}
 		
 //		Socket socket;
 //		try {
