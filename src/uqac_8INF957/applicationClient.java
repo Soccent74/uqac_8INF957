e57234951fcc9d71fd6de0114780b77ea6b21c44
Benjamin <bourgeaux.benj@gmail.com>
2016-02-13 13:27:25 -0500

    Ajout lecture du fichier commandes.txt

--- src/uqac_8INF957/applicationClient.java
+++ src/uqac_8INF957/applicationClient.java
@@ -4,20 +4,48 @@
 package uqac_8INF957;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 /**
  * @author Benjamin Bourgeaux et Lucas Hélaine
  *
  */
 public class applicationClient {
 	public static void main(String[] zero) {
-		Socket socket;
-		try {
-			socket = new Socket(InetAddress.getLocalHost(),2009);	
-			socket.close();
-		}catch (UnknownHostException e) {
-			e.printStackTrace();
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
+		File streamCommande = null;
+//	    String filePath = "commandes.txt";
+
+	    
+	    // On test si on peut ouvrir le fichier, ainsi que si il n'y a pas de problème avec les entrées/sorties. 
+		try { 
+	    	streamCommande = new File("commandes.txt");
+	    	Scanner scanner=new Scanner(streamCommande);
+	    	 
+	    	// On boucle sur chaque champ detecté
+	    	while (scanner.hasNextLine()) {
+	    	    String line = scanner.nextLine();
+	    	    System.out.println(line);
+	    		//faites ici votre traitement
+	    	    
+	    	    
+	    	    
+	    	}
+	    	scanner.close();
+	    }catch(FileNotFoundException e) {
+	         // Cette exception est levée si l'objet FileInputStream ne trouve aucun fichier
+	         e.printStackTrace();
+	    }catch(IOException e) {
+	         // Celle-ci se produit lors d'une erreur d'écriture ou de lecture
+	         e.printStackTrace();
+	    }
+		
+//		Socket socket;
+//		try {
+//			socket = new Socket(InetAddress.getLocalHost(),2009);	
+//			socket.close();
+//		}catch (UnknownHostException e) {
+//			e.printStackTrace();
+//		}catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
