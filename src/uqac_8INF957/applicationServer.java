15cb0726a48a613e858b582dcd82b8c6769897ec
Lucas <lucas.helainemenard@hotmail.fr>
2016-02-13 22:08:37 -0500

    Modification serveur
    ... Il avait les yeux bleus, ...

--- src/uqac_8INF957/applicationServer.java
+++ src/uqac_8INF957/applicationServer.java
@@ -2,27 +2,143 @@
  * 
  */
 package uqac_8INF957;
+import uqac_8INF957.Commande;
+
+import java.io.*;
 import java.io.IOException;
 import java.net.*;
+import java.util.*;
 /**
  * @author Benjamin Bourgeaux et Lucas Hélaine
  *
  */
-public class applicationServer {
-	
-	public static void main(String[] zero) {
-		ServerSocket socketserver  ;
-		Socket socketduserveur ;
+public class applicationServer{
+
+	public applicationServer(int port){ //prend le numéro de port, crée un SocketServer sur le port
 		try {
-	    	socketserver = new ServerSocket(2009);
-	    	socketduserveur = socketserver.accept(); 
-	    	System.out.println("Un zéro s'est connecté !");
-	        socketserver.close();
-	        socketduserveur.close();
+	    	socketserver = new ServerSocket(port);
+	    	System.out.println("Socket créé");
+	    	aVosOrdres();
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+    /**
+    * Se met en attente de connexions des clients. Suite aux connexions, elle lit
+    * ce qui est envoyé à travers la Socket, recrée l’objet Commande envoyé par
+    * le client, et appellera traiterCommande(Commande uneCommande)
+    */         
+    public void aVosOrdres() {
+    	ServerSocket socketserver;
+		Socket socketduserveur;
+    	final BufferedReader in;
+    	final PrintWriter out;
+    	final Scanner sc=new Scanner(System.in);
+    	try {
+    	    socketduserveur = socketserver.accept();
+    	    System.out.println("Connexion effectue");
+    	    out = new PrintWriter(socketduserveur.getOutputStream());
+    	    in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream()));
+    	    String s;
+    	    s = sc.next();
+    	    out.println(s);
+    	    out.flush();    
+    	    String message_client;
+    	    message_client = in.readLine();
+    	    System.out.println("Commande : "+message_client);
+    	    socketserver.close();
+            socketduserveur.close();
+    	    }
+    	catch (IOException e) {
+    	  e.printStackTrace();
+    	}
+        
+    }
+    
+   /**
+    * prend une Commande dument formattée, et la traite. Dépendant du type de commande, 
+    * elle appelle la méthode spécialisée
+    */
+    public void traiteCommande(Commande uneCommande) {
+    	String nom_commande;
+    	nom_commande = uneCommande.getType_commande();
+
+    	if(nom_commande.equals("compilation")){
+    		
+    	}
+    	else if(nom_commande.equals("chargement")){
+    		
+    	}
+    	else if(nom_commande.equals("creation")){
+
+    	}
+    	else if(nom_commande.equals("lecture")){
+    		//traiterLecture(Object pointeurObjet, uneCommande.getNom_attribut());
+    	}
+    	else if(nom_commande.equals("ecriture")){
+    		//traiterEcriture(Object pointeurObjet, uneCommande.getNom_attribut(), Object valeur)
+    	}
+    	else if(nom_commande.equals("fonction")){
+
+    	}
+    }
+    
+    /**
+    * traiterLecture : traite la lecture d’un attribut. Renvoies le résultat par le 
+	* socket
+    */
+    public void traiterLecture(Object pointeurObjet, String attribut) {
+    
+    }
+    
+    /**
+    * traiterEcriture : traite l’écriture d’un attribut. Confirmes au client que l’écriture
+	* s’est faite correctement.
+    */
+    public void traiterEcriture(Object pointeurObjet, String attribut, Object valeur) {
+    	
+    }
+
+    /**
+    * traiterCreation : traite la création d’un objet. Confirme au client que la création
+	* s’est faite correctement.
+    */
+    public void traiterCreation(Class classeDeLobjet, String identificateur) {
+    	
+    }
+
+    /**
+    * traiterChargement : traite le chargement d’une classe. Confirmes au client que la création
+	* s’est faite correctement.
+    */
+    public void traiterChargement(String nomQualifie) {
+    	
+    }
+
+    /**
+    * traiterCompilation : traite la compilation d’un fichier source java. Confirme au client
+	* que la compilation s’est faite correctement. Le fichier source est donné par son chemin
+	* relatif par rapport au chemin des fichiers sources.
+    */
+    public void traiterCompilation(String cheminRelatifFichierSource) {
+    	
+    }
+
+    /**
+    * traiterAppel : traite l’appel d’une méthode, en prenant comme argument l’objet
+	* sur lequel on effectue l’appel, le nom de la fonction à appeler, un tableau de nom de 
+	* types des arguments, et un tableau d’arguments pour la fonction. Le résultat de la 
+	* fonction est renvoyé par le serveur au client (ou le message que tout s’est bien 
+	* passé)
+    **/
+    public void traiterAppel(Object pointeurObjet, String nomFonction, String[] types, 
+Object[] valeurs) {
+    	
+    }
+
 	
+	public static void main(String[] arg) {
+		applicationServer serv = new applicationServer(2009);
     }// Fin du main.
 	
 }
